5610ddef9c0fd57d7185965b9ffd60fbc8cf985b
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-30 22:36:50 -0300

    ex compras

--- src/compra.java
+++ src/compra.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class compra {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.printf("Digite o preço da compra: ");
+        double cmp = scn.nextDouble();
+        if (cmp > 1000){
+            System.out.println("VocÊ recebeu 15% de desconto");
+            double desc = cmp * 0.75;
+            System.out.println("o total foi: " + desc);
+        }
+        else {
+            System.out.println("VosÊ recebeu 8% de desconto");
+            double desc = cmp * 0.92;
+            System.out.println("O total foi: " + desc);
+        }
+    }
 }
