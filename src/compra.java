f5625ba3ef5b4a5bb807bb2cb3291746232911b6
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-30 22:39:14 -0300

    correção compra

--- src/compra.java
+++ src/compra.java
@@ -7,7 +7,7 @@ public class compra {
         double cmp = scn.nextDouble();
         if (cmp > 1000){
             System.out.println("VocÊ recebeu 15% de desconto");
-            double desc = cmp * 0.75;
+            double desc = cmp * 0.85;
             System.out.println("o total foi: " + desc);
         }
         else {
